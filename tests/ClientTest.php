a95b37b7fa20a9275d2eb8fdf45c9587a0662fd5
Sebastian Grodzicki <sebastian@grodzicki.pl>
2023-04-17 10:46:44 +0200

    Test `set_site`

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,12 +2,36 @@
 
 namespace UniFi_API\Tests;
 
+use UniFi_API\Client;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
-    public function testSomething()
-    {
+    private $client;
 
+    protected function setUp()
+    {
+        $this->client = new Client('unifi', 'unifi');
+    }
+
+    public function testSetSite()
+    {
+        // default
+        $this->assertEquals('default', $this->client->get_site());
+
+        // custom
+        $this->client->set_site('foobar');
+        $this->assertEquals('foobar', $this->client->get_site());
+
+        // whitespace
+        $this->client->set_site('   foobar ');
+        $this->assertEquals('foobar', $this->client->get_site());
+
+        // whitespace (debug mode)
+        $this->client->set_debug(true);
+        $this->expectException(\PHPUnit_Framework_Error_Notice::class);
+        $this->expectExceptionCode(E_USER_NOTICE);
+        $this->expectExceptionMessage('The provided (short) site name may not contain any spaces');
+        $this->client->set_site('   foobar ');
     }
 }
